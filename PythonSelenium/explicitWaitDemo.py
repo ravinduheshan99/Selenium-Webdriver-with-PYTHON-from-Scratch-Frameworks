5fb60152ed7b6e653697a5644b5c1e5278aa020a
ravinduheshan99 <ravinduheshan99@gmail.com>
2026-04-09 21:41:36 +0530

    add Selenium synchronization with implicit and explicit waits and end-to-end test example Covers wait strategies, handling dynamic elements, and implementing an ecommerce flow using proper synchronization

---
+++ PythonSelenium/explicitWaitDemo.py
@@ -0,0 +1,40 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+current_dir = os.path.dirname(__file__)
+dirver_path = os.path.join(current_dir,"..","resources","chromedriver-win64","chromedriver.exe")
+driver_path = os.path.abspath(dirver_path)
+service_obj = Service(driver_path)
+driver = webdriver.Chrome(service=service_obj)
+driver.implicitly_wait(5)
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/")
+
+driver.find_element(By.CSS_SELECTOR,".search-keyword").send_keys("ber")
+time.sleep(5)
+
+filteredProductList = driver.find_elements(By.XPATH,"//div[@class='products']/div")
+filteredProductsCount = len(filteredProductList)
+assert filteredProductsCount > 0
+
+for product in filteredProductList:
+    product.find_element(By.XPATH,"div/button").click()
+
+driver.find_element(By.CSS_SELECTOR,"img[alt='Cart']").click()
+driver.find_element(By.XPATH,"//button[text()='PROCEED TO CHECKOUT']").click()
+
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR,".promoCode")))
+
+driver.find_element(By.CSS_SELECTOR,".promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR,".promoBtn").click()
+
+wait.until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"promoInfo")))
+
+promoCodeSuccessText = driver.find_element(By.CLASS_NAME,"promoInfo").text
+assert promoCodeSuccessText == "Code applied ..!"
